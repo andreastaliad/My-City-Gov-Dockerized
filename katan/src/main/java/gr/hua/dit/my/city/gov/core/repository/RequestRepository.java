38f15e63d1e71327cf395e7dd4795e936bb63237
Ηλίας Πουλέρης <poulerhshlias@gmail.com>
2025-12-11 14:53:41 +0200

    εφτιαξα το repository για τα αιτηματα

--- katan/src/main/java/gr/hua/dit/my/city/gov/core/repository/RequestRepository.java
+++ katan/src/main/java/gr/hua/dit/my/city/gov/core/repository/RequestRepository.java
@@ -1,4 +1,7 @@
 package gr.hua.dit.my.city.gov.core.repository;
 
-public class RequestRepository {
-}
+import gr.hua.dit.my.city.gov.core.model.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequestRepository extends JpaRepository<Request, Long> {}
+
