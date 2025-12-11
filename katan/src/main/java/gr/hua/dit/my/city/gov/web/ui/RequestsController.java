fa9daf953a77611f3feddd74fdfa9910c2aebee7
Ηλίας Πουλέρης <poulerhshlias@gmail.com>
2025-12-11 14:54:55 +0200

    controller για τα αιτηματα οπως στα ραντεβου

--- katan/src/main/java/gr/hua/dit/my/city/gov/web/ui/RequestsController.java
+++ katan/src/main/java/gr/hua/dit/my/city/gov/web/ui/RequestsController.java
@@ -1,14 +1,27 @@
 package gr.hua.dit.my.city.gov.web.ui;
 
+import gr.hua.dit.my.city.gov.core.repository.RequestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import gr.hua.dit.my.city.gov.core.model.Request;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RequestsController {
 
+    @Autowired
+    private RequestRepository requestRepository;
+
     @GetMapping("/requests/form")
     public String showRequestsForm() {
         return "requests-form :: content";
+    }
+
+    @PostMapping("/requests")
+    public String saveRequest(Request request) {
+        requestRepository.save(request);
+        return "requests-success :: content";
     }
 }
 
