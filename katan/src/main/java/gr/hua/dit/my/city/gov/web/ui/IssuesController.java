1389ccc0fff78a0e6b4e7fd57ac308599f5fa4a5
Ηλίας Πουλέρης <poulerhshlias@gmail.com>
2025-12-11 15:43:54 +0200

    controller για τα προβληματα

--- katan/src/main/java/gr/hua/dit/my/city/gov/web/ui/IssuesController.java
+++ katan/src/main/java/gr/hua/dit/my/city/gov/web/ui/IssuesController.java
@@ -1,14 +1,27 @@
 package gr.hua.dit.my.city.gov.web.ui;
 
+import gr.hua.dit.my.city.gov.core.model.Issue;
+import gr.hua.dit.my.city.gov.core.repository.IssueRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class IssuesController {
 
+    @Autowired
+    private IssueRepository issueRepository;
+
     @GetMapping("/issues/form")
     public String showIssuesForm() {
         return "issues-form :: content";
+    }
+
+    @PostMapping("/issues")
+    public String saveIssue(Issue issue) {
+        issueRepository.save(issue);
+        return "issues-success :: content";
     }
 }
 
