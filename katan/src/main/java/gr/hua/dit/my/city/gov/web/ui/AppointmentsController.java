6651c164a169c11653272e3bd3026c8a4677c199
Ηλίας Πουλέρης <poulerhshlias@gmail.com>
2025-12-11 11:56:48 +0200

    τροποποιησεις για να γινονται σωστες εγγραφες μεσω ajax σε repository

--- katan/src/main/java/gr/hua/dit/my/city/gov/web/ui/AppointmentsController.java
+++ katan/src/main/java/gr/hua/dit/my/city/gov/web/ui/AppointmentsController.java
@@ -1,13 +1,27 @@
 package gr.hua.dit.my.city.gov.web.ui;
 
+import gr.hua.dit.my.city.gov.core.model.Appointment;
+import gr.hua.dit.my.city.gov.core.repository.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AppointmentsController {
 
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
     @GetMapping("/appointments/form")
     public String showAppointmentForm() {
         return "appointments-form :: content";
+    }
+
+    @PostMapping("/appointments")
+    public String saveAppointment(Appointment appointment) {
+        //για αποθήκευση στη βάση
+        appointmentRepository.save(appointment);
+        return "appointments-success :: content";
     }
 }
